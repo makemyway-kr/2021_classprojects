2a975e29c7a8639a597ff0e876d998359e128483
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-03-25 17:51:54 +0900

    file_processing_hw

--- copy.c
+++ copy.c
@@ -9,13 +9,9 @@ void read(char* filename,char* copyfile){
     while(feof(original_file)==0)
     {
         char temp[10]={};
-        int amount_of_copied_letter=fread(temp,sizeof(char),10,original_file);//10바이트 보다 작더라도 저장해 주기 위해서 읽은 글자의 수를 변수로 저장함.
+        int amount_of_copied_letter=fread(temp,sizeof(char),ten,original_file);//10바이트 보다 작더라도 저장해 주기 위해서 읽은 글자의 수를 변수로 저장함.
         fwrite(temp,sizeof(char),amount_of_copied_letter,copy_file);//복사
     }
     fclose(original_file);
     fclose(copy_file);
-}
-int main(void)
-{
-    read("hi.txt","hello.txt");
 }
