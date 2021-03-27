119a1adcde51a7d1ab548912d6d693e2b4905c2f
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-03-27 15:09:16 +0900

    file processing
    wow finished insert.c

--- insert/insert.c
+++ insert/insert.c
@@ -5,32 +5,17 @@ void insert(char* filename,int offset,char*data)
 {
     FILE*mod_file=fopen(filename,"r+w");//읽기와 쓰기로 오픈
     fseek(mod_file,0,SEEK_END);//파일의 크기 확인
-    int size_of_file=ftell(mod_file);//
-    char*temp=malloc(sizeof(char)*(size_of_file-offset-1));//뒤의 문장 저장할 임시 공간
-    fseek(mod_file,offset+1,SEEK_SET);
-    fread(temp,sizeof(temp),1,mod_file);
-    char *td=malloc(sizeof(char)*(size_of_file-offset-1));//temporary data->data의 내용중 앞부분을 자름.
-    int flag=sizeof(td)/sizeof(char);
-     for(int i=0;i<sizeof(td)/sizeof(char);i++)
-    {
-        td[i]=data[i];
-    }
-    char *td2=malloc(sizeof(char)*(strlen(data)-strlen(td)));
-    for(int i=0;i<sizeof(td2)/sizeof(char);i++)
-    {
-        td2[i]=data[flag+i];
-    }  
-    fseek(mod_file,offset,SEEK_SET);
-    fwrite(td,sizeof(td),1,mod_file);
-    fclose(mod_file);
-    free (td);
-    FILE*mo_file=fopen(filename,"a");
-    fwrite(td2,sizeof(td2),1,mo_file);
-    free(td2);
-    fclose(mo_file);
-    
+    int size_of_file=ftell(mod_file);//파일의 크기를 담음.
+    char* temp=malloc(sizeof(char)*(size_of_file-offset));//오프셋+1부터 끝까지의 문자열을 저장할 임시 공간
+    fseek(mod_file,offset+1,SEEK_SET);//오프셋 +1로 포인터 이동
+    fread(temp,sizeof(temp),1,mod_file);//오프셋+1부터 끝까지의 문자열 저장.
+    fseek(mod_file,offset+1,SEEK_SET);//오프셋+1로 포인터 이동.
+    fputs(data,mod_file);//집어넣을 데이터 쓰기
+    fputs(temp,mod_file);//기존 offset+1부터 있던 데이터 입력
+    free(temp);
+    fclose(mod_file);    
 }
 int main(void)
 {
-    insert("hi.txt",2,"aaaaa");
+    insert("hi.txt",2,"2222");
 }
